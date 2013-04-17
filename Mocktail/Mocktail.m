4417992e3b402d1adbc4df77dd1bade1b07ce55e
Ben Coffman <ben.coffman@capitalone.com>
2013-04-17 09:54:23 -0400

    Location of changes

--- Mocktail/Mocktail.m
+++ Mocktail/Mocktail.m
@@ -182,6 +182,7 @@ static NSMutableSet *_allMocktails;
     MocktailResponse *response = [MocktailResponse new];
     response.mocktail = self;
     response.methodRegex = [NSRegularExpression regularExpressionWithPattern:lines[0] options:NSRegularExpressionCaseInsensitive error:nil];
+    // --
     NSString *escapedURL  = [NSRegularExpression escapedPatternForString:lines[1]] ;
     response.absoluteURLRegex = [NSRegularExpression regularExpressionWithPattern:escapedURL options:NSRegularExpressionCaseInsensitive error:nil];
     response.statusCode = [lines[2] integerValue];
