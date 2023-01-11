7d9084371f07364bec6213d5109ae14885d08e29
Dmytro Chapovskyi <dchapovskyi@shutterstock.com>
2023-01-11 09:33:32 +0100

    macos support removed

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Mocktail",
     platforms: [
-        .macOS(.v10_14), .iOS(.v13), .tvOS(.v13)
+        .iOS(.v13), .tvOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
